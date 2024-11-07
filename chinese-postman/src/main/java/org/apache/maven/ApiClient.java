99c4b478cd3a00f71dac35b8b343e66e6f010508
MickeiasGA <115158113+MickeiasGA@users.noreply.github.com>
2024-11-07 17:44:24 -0300

    APIClient changes
    - changed getIntersections
    - tested changes
    - no results

--- chinese-postman/src/main/java/org/apache/maven/ApiClient.java
+++ chinese-postman/src/main/java/org/apache/maven/ApiClient.java
@@ -66,13 +66,20 @@ public class ApiClient {
 
     public static Set<String> getIntersections(List<String> streets) throws Exception {
         Set<String> intersections = new HashSet<>();
+        Set<String> checkedPairs = new HashSet<>(); // Armazena pares únicos de ruas
 
         for (int i = 0; i < streets.size(); i++) {
             for (int j = i + 1; j < streets.size(); j++) {
                 String street1 = streets.get(i);
                 String street2 = streets.get(j);
-                
+
+                // Cria uma chave única para cada par de ruas, ignorando a ordem
+                String pairKey = street1.compareTo(street2) < 0 ? street1 + "-" + street2 : street2 + "-" + street1;
+                if (checkedPairs.contains(pairKey)) continue; // Ignora pares já verificados
+                checkedPairs.add(pairKey);
+
                 String address = street1.replace(" ", "+") + "+%26+" + street2.replace(" ", "+");
+                
                 String url = String.format(
                     "https://maps.googleapis.com/maps/api/geocode/json?address=%s&type=route&key=%s",
                     address, API_KEY
@@ -81,21 +88,36 @@ public class ApiClient {
                 Request request = new Request.Builder().url(url).build();
                 
                 try (Response response = client.newCall(request).execute()) {
-                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                    if (!response.isSuccessful()) {
+                        System.out.println("Erro na resposta da API para o cruzamento de " + street1 + " e " + street2);
+                        continue;
+                    }
 
                     String jsonResponse = response.body().string();
                     var jsonObject = new JSONObject(jsonResponse);
 
-                    if (jsonObject.getJSONArray("results").length() > 0) {
-                        JSONObject location = jsonObject.getJSONArray("results")
-                            .getJSONObject(0)
+                    JSONArray results = jsonObject.optJSONArray("results");
+
+                    JSONObject result = results.getJSONObject(0);
+                    String formattedAddress = result.optString("formatted_address", "");
+                    System.out.println(formattedAddress);
+                    System.out.println();
+                    
+                    // Confirma se o endereço contém os nomes de ambas as ruas
+                    if (formattedAddress.contains(street1.replace("Rua", "")) && formattedAddress.contains(street2.replace("Rua", ""))) {
+                        JSONObject location = result
                             .getJSONObject("geometry")
                             .getJSONObject("location");
                         
                         double lat = location.getDouble("lat");
                         double lng = location.getDouble("lng");
                         intersections.add(String.format("Intersection of %s and %s at coordinates: (%f, %f)", street1, street2, lat, lng));
+                        System.out.println("Cruzamento encontrado: " + street1 + " & " + street2 + " em (" + lat + ", " + lng + ")");
+                    } else {
+                        System.out.println("Endereço formatado não contém ambas as ruas para " + street1 + " e " + street2);
                     }
+                } catch (Exception e) {
+                    System.out.println("Erro ao processar o cruzamento de " + street1 + " e " + street2 + ": " + e.getMessage());
                 }
             }
         }
@@ -105,7 +127,7 @@ public class ApiClient {
 
     public static void main(String[] args) throws Exception {
         double[] coords = getCoordinates("R. Angelina Cury Abdalla, 146 - Jardim Fernanda, Campinas - SP");
-        List<String> streets = getStreets(coords[0], coords[1], 1000);
+        List<String> streets = getStreets(coords[0], coords[1], 200);
     
         System.out.println("Streets in the area:");
         for (String street : streets) {
@@ -114,6 +136,7 @@ public class ApiClient {
 
         Set<String> intersections = getIntersections(streets);
         System.out.println("\nIntersections:");
+        System.out.println(intersections.size());
         for (String intersection : intersections) {
             System.out.println(intersection);
         }
